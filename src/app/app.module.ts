41a8df311f0aabf0a30625ef1299a98467e3e057
Khushboo603 <rishikasinha786@gmail.com>
2024-10-14 15:07:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssociatelistingComponent } from './component/associatelisting/associatelisting.component';
+import { AddassociateComponent } from './component/addassociate/addassociate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssociatelistingComponent,
+    AddassociateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associatelisting',
+  templateUrl: './associatelisting.component.html',
+  styleUrls: ['./associatelisting.component.scss']
+})
+export class AssociatelistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
